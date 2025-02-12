9307389dad4cc92d2c914bfb38e3c9bf8c724415
Atle Haugan <atleh@equinor.com>
2025-02-12 10:48:51 +0100

    Octokit Graphql

--- octokit-test.mjs
+++ octokit-test.mjs
@@ -3,8 +3,14 @@ import { Octokit, App } from "octokit";
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
+const query = `{
+    viewer {
+      login
+    }
+  }`;
 // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
 const {
-  data: { login },
-} = await octokit.rest.users.getAuthenticated();
-console.log("Hello, %s", login);
+    viewer: { login },
+  } = await octokit.graphql(query);
+
+  console.log("Hello, %s", login);
