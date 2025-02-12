bf653d7da53d08867f3647845ff56456c9502e36
Atle Haugan <atleh@equinor.com>
2025-02-12 10:51:23 +0100

    Octokit query repositoryOwner

--- octokit-test.mjs
+++ octokit-test.mjs
@@ -3,14 +3,31 @@ import { Octokit, App } from "octokit";
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-const query = `{
-    viewer {
-      login
+const query = `
+{
+    repositoryOwner(login: "equinor") {
+        repositories(
+            first: 100
+            ownerAffiliations: [OWNER]
+            isFork: false
+            isLocked: false
+            orderBy: { field: UPDATED_AT, direction: DESC }
+        ) {
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            nodes {
+                name
+            }
+        }
     }
-  }`;
-// Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
+}
+`;
+
+  // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
 const {
-    viewer: { login },
+    repositoryOwner: { repositories },
   } = await octokit.graphql(query);
 
-  console.log("Hello, %s", login);
+  console.log(repositories);
