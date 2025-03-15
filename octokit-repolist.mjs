a8ba65e3d26535b64dabf8652129667ea3e627e5
Atle Haugan <atleh@equinor.com>
2025-03-15 12:04:56 +0100

    Support paging

--- octokit-repolist.mjs
+++ octokit-repolist.mjs
@@ -3,30 +3,45 @@ import { graphql } from '@octokit/graphql';
 const userName = "atleh";
 
 const query = `
-{
-  repositoryOwner(login: "equinor") {
-    repositories(
-      visibility: INTERNAL,
-      first: 5,
-    ) {
-      nodes {
-        name
-      }
-      pageInfo {
-        endCursor
-        hasNextPage
+  query ($cursor: String) {
+    repositoryOwner(login: "equinor") {
+      repositories(
+        visibility: INTERNAL,
+        first: 5,
+        after: $cursor
+      ) {
+        nodes {
+          name
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
       }
     }
-  }
-}`;
+  }`;
 
 const gqlEndpoint = graphql.defaults({
     headers: {
       authorization: `token ${process.env.GITHUB_TOKEN}`,
     },
   });
-const result = await gqlEndpoint(query);
-for (const node of result.repositoryOwner.repositories.nodes) {
-  console.log(node.name);
+
+let cursor = null;
+let hasNextPage = true;
+
+let i = 0;
+while (hasNextPage) {
+  const result = await gqlEndpoint(query, { cursor });
+  for (const node of result.repositoryOwner.repositories.nodes) {
+    console.log(node.name);
+  }
+  console.log(result.repositoryOwner.repositories.pageInfo);
+
+  cursor = result.repositoryOwner.repositories.pageInfo.endCursor;
+  hasNextPage = result.repositoryOwner.repositories.pageInfo.hasNextPage;
+  i++;
+  if (i > 5) {
+    break;
+  }
 }
-console.log(result.repositoryOwner.repositories.pageInfo);
