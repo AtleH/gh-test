02ef712fdfed48b2b28913b08c65d2e6df8d34d0
Atle Haugan <atleh@equinor.com>
2025-03-15 12:01:07 +0100

    Add pageInfo

--- octokit-repolist.mjs
+++ octokit-repolist.mjs
@@ -4,21 +4,17 @@ const userName = "atleh";
 
 const query = `
 {
-  repositoryOwner(login:"equinor") {
+  repositoryOwner(login: "equinor") {
     repositories(
-      first:5
-    )
-    {nodes {
-      name
-        collaborators(first: 10) {
-          nodes {
-            login
-            name
-          }
-          edges {
-            permission
-          }
-        }
+      visibility: INTERNAL,
+      first: 5,
+    ) {
+      nodes {
+        name
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
       }
     }
   }
@@ -30,26 +26,7 @@ const gqlEndpoint = graphql.defaults({
     },
   });
 const result = await gqlEndpoint(query);
-for (const repo of result.repositoryOwner.repositories.nodes) {
-    console.log(repo.name);
-    const collaborators = repo.collaborators.nodes.map((node, index) => ({
-      ...node,
-      permission: repo.collaborators.edges[index]?.permission
-    }));
-    const filtered = collaborators.filter(collaborator =>
-      (collaborator.permission === 'ADMIN' && collaborator.name !== 'Equinor Admin' && collaborator.name !== 'Equinor-Admin')
-    );
-    for (const collaborator of collaborators) {
-      console.log(`  ${collaborator.name} (${collaborator.login}) - ${collaborator.permission}`);
-    }
+for (const node of result.repositoryOwner.repositories.nodes) {
+  console.log(node.name);
 }
-// console.log(result.repositoryOwner.repositories.nodes);
-// const myRepositories = result.repositories.nodes.filter(repo => {
-//     const isOrganizationRepo = repo => repo.repository.owner.login === "equinor";
-//     const isArchived = repo => repo.repository.isArchived === true;
-//     const isLoggedInUser = c => c.node.login.toLowerCase() === userName.toLowerCase();
-//     const collaborators = repo.repository.collaborators.edges;
-//     return isOrganizationRepo(repo) && !isArchived(repo) && collaborators.some(isLoggedInUser);
-// });
-
-// console.log(myRepositories);
+console.log(result.repositoryOwner.repositories.pageInfo);
